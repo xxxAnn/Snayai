d84152dc8ae2bf56199af530e72fe41b5c2ccb23
キャンド <amehikoji@gmail.com>
2023-01-15 04:28:11 -0500

    simplify bat file

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @if not exist "./build" (
     @mkdir build >nul
 )
-@g++ -o build/snayai.exe -Wl,-Bstatic external/sfml-graphics-2.dll external/sfml-system-2.dll external/sfml-window-2.dll src/* -Wl,--as-needed -ID:\Documents\Code\Personal\SFML\include >nul
+@g++ -o build/snayai.exe -Wl,-Bstatic external/* src/* -Wl,--as-needed -ID:\Documents\Code\Personal\SFML\include >nul
 @cp -fr ./external/* ./build
