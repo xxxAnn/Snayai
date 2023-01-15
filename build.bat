dd6f1a44260a1a835765660db26c30b981da32ca
キャンド <amehikoji@gmail.com>
2023-01-15 00:36:24 -0500

    change cp to xcopy

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
     @mkdir build >nul
 )
 @g++ -o build/snayai.exe -Wl,-Bstatic external/sfml-graphics-2.dll external/sfml-system-2.dll external/sfml-window-2.dll src/main.cpp -Wl,--as-needed -ID:\Documents\Code\Personal\SFML\include >nul
-@cp -fr ./external/* ./build
+@xcopy ./external/* ./build /y
