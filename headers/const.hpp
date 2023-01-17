7fb8c1750d78b82c4e25f6967ba84faaab916fca
キャンド <amehikoji@gmail.com>
2023-01-17 02:04:59 -0500

    dumb comment

--- headers/const.hpp
+++ headers/const.hpp
@@ -3,6 +3,7 @@
 
 namespace snayai {
     namespace constants {
+        // i have no idea what the units are
         static const int WINDOW_WIDTH = 640;
         static const int WINDOW_HEIGHT = 480;
         static const int TILE_SIZE = 28;
